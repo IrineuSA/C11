ff43205cfc293cffb247b00f74adad923a93184e
Irineu <irineo.rocha@gmail.com>
2025-08-09 20:31:31 -0300

    Delete ex-1-2.py

--- ex-1-2.py
+++
@@ -1,8 +0,0 @@
-#2. Mostre a tabuada de um número que o usuário escolher dentro de um
-#intervalo específico também escolhido por ele
-
-import math
-n = int (input("Número para tabuada: "))
-intervalo = int (input("intervalo: "))
-for i in range(1, intervalo + 1):
-    print(f"{n} x {i} = {n * i}")
