c18027216babcad17722601b774e0ffc0aecfa39
Irineu <irineo.rocha@gmail.com>
2025-09-07 22:19:06 -0300

    Upload dataset

--- PV1/pv1.py
+++ PV1/pv1.py
@@ -1,2 +1,12 @@
 import numpy as np
-ds = np.loadtxt('PV1/Data/*DATASET-AQUI*',delimiter=';',dtype=str,encoding='utf-8')
+ds = np.loadtxt('F:\\Users\\Rockmore\\Documentos\\Inatel\\25-2\\C11\\PV1\\Data\\shopping_trends.csv',delimiter=',',dtype=str,encoding='utf-8')
+
+#1 Mostre qual a média de idade dos homens presentes neste dataset;
+
+#2 Mostre quantos clientes gastaram mais que a média de gastos das compras deste dataset;
+
+#3 Mostre qual é a porcentagem de vendas do item menos vendido da loja;
+
+#4 Mostre qual a porcentagem de vendas que tiveram algum tipo de desconto;
+
+#5 Mostre qual a cor de roupa mais popular no verão segundo este dataset
