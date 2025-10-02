35beff51440ea5e0de902cf08ea83554d63f7563
Irineu <irineo.rocha@gmail.com>
2025-10-01 23:33:35 -0300

    Refatora Ex cap 6 pt. 1

--- Cap-6/cap-6-1.py
+++ Cap-6/cap-6-1.py
@@ -3,12 +3,18 @@
 #(Deathrate) e outro a taxa de natalidade (Birthrate) dos países da
 #América do Norte;
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 dPaises = pd.read_csv('C:\\Users\\Irineu\\Documents\\Projetos\\C11\\Cap-6\\Data\\paises.csv',delimiter=';')
-dPaises_NA = dPaises[dPaises['Region'] == 'NORTHERN AMERICA']
-plt.plot(dPaises_NA['Country'], dPaises_NA['Deathrate'], 'r-', label='Taxa de Mortalidade')
-plt.plot(dPaises_NA['Country'], dPaises_NA['Birthrate'], 'b--', label='Taxa de Natalidade')
-plt.xlabel('Países da América do Norte')
+dPaisesNA = dPaises[dPaises["Region"].str.contains("NORTHERN AMERICA")]
+
+plt.plot(dPaisesNA["Country"], dPaisesNA["Birthrate"], marker='o', label="Taxa de Natalidade")
+plt.plot(dPaisesNA["Country"], dPaisesNA["Deathrate"], marker='s', label="Taxa de Mortalidade")
+plt.title("Taxa de Natalidade e Mortalidade - América do Norte")
+plt.xlabel("Países")
+plt.ylabel("Taxa (%)")
+plt.legend()
+plt.grid(True, linestyle="--", alpha=0.5)
+plt.tight_layout()
+plt.show()
